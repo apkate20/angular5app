ea959ef75e3de3b5ff21c3e14a484ee1820a8166
Apeksha Dekate <apeksha.dekate@provintl.com>
2023-07-27 16:23:48 +0530

    commit 2

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { BlogComponent } from './components/blog/blog.component';
-import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from '../components/home/home.component';
+import { BlogComponent } from '../components/blog/blog.component';
+import { AboutComponent } from '../components/about/about.component';
 
 const appRoutes: Routes = [
   {path: 'home', component: HomeComponent},
@@ -15,4 +15,3 @@ const appRoutes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-
